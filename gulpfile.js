483c45d76a01378647372f9fce58e26f10350058
Mateo <mateojm94@gmail.com>
2018-03-20 19:54:19 -0500

    It was added gulp task for injecting bower dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,32 @@ var gulp = require('gulp'),
     bowerFiles = require('main-bower-files'),
     inject = require('gulp-inject');
 
+var base_url = './src/client/kanban/',
+    paths = {
+        index: base_url + 'index.html',
+        dest: './dist/client/'
+    };
+
 gulp.task('default', function() {
     console.log("Not implemented yet.")
 });
+
+gulp.task('inject-dependencies', injectFiles)
+
+/**
+ * @function injectFiles
+ * @description Inject all the dependencies into index.html
+ */
+function injectFiles() {
+    injectBowerFiles();
+}
+
+/**
+ * @function injectBowerFiles
+ * @description 
+ */
+function injectBowerFiles() {
+    gulp.src(paths.index)
+        .pipe(inject(gulp.src(bowerFiles(), {read: false}), {name: 'bower'}))
+        .pipe(gulp.dest(paths.dest));
+}
