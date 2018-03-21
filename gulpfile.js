6c7e09580c500b036d96bff53bca6a72579c54cd
Brahian Zapata <brahian.zapata@hotmail.com>
2018-03-20 20:59:56 -0500

    Install dependence gulp-less

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,15 @@
 var gulp = require('gulp'),
     bowerFiles = require('main-bower-files'),
-    inject = require('gulp-inject');
+    inject = require('gulp-inject'),
+    styleCss = require('gulp-less');
 
 var base_url = './src/client/kanban/',
     paths = {
         index: base_url + 'index.html',
-        dest: './dist/client/'
+        dest: './dist/client/',
+        less_style: 'assets/styles/*.less',
+        style_dest: base_url + 'style_css'
+
     };
 
 gulp.task('default', function() {
@@ -13,6 +17,8 @@ gulp.task('default', function() {
 });
 
 gulp.task('inject-dependencies', injectFiles)
+
+gulp.task('gulp-less', injectLess);
 
 /**
  * @function injectFiles
@@ -23,6 +29,14 @@ function injectFiles() {
 }
 
 /**
+ * @function injectLess
+ * @description Inject all the dependencies into css
+ */
+function injectLess() {
+    injectStyleLess();
+}
+
+/**
  * @function injectBowerFiles
  * @description 
  */
@@ -30,4 +44,14 @@ function injectBowerFiles() {
     gulp.src(paths.index)
         .pipe(inject(gulp.src(bowerFiles(), {read: false}), {name: 'bower'}))
         .pipe(gulp.dest(paths.dest));
+}
+
+/**
+ * @function injectStyleLess
+ * @description 
+ */
+function injectStyleLess() {
+    gulp.src(paths.less_style)
+        .pipe(inject(gulp.src(styleCss(), {read: false}), {name: 'bower'}))
+        .pipe(gulp.dest(paths.style_dest));
 }
